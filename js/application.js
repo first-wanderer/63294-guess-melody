18e3244fc15414d716bf87ed3bf1416856f8af4c
First Wanderer <kaa-master37@mail.ru>
2018-04-28 10:02:01 +0300

    Move toggle screen to static method

--- js/application.js
+++ js/application.js
@@ -5,28 +5,28 @@ import GameModel from './game-model';
 
 const mainContainer = document.querySelector(`.app .main`);
 
-const toggleScreen = (nextPage) => {
-  if (nextPage) {
-    mainContainer.innerHTML = ``;
-    mainContainer.appendChild(nextPage);
-  }
-};
-
 export default class Application {
   static showWelcome() {
     const welcomePage = new WelcomePage(Application.showGame);
-    toggleScreen(welcomePage.element);
+    Application._toggleScreen(welcomePage.element);
   }
 
   static showGame() {
     const model = new GameModel();
     const gamePage = new GamePage(model, Application.showResult);
-    toggleScreen(gamePage.element);
+    Application._toggleScreen(gamePage.element);
     gamePage.startGame();
   }
 
   static showResult(userResult) {
     const resultPage = new ResultPage(userResult, Application.showGame);
-    toggleScreen(resultPage.element);
+    Application._toggleScreen(resultPage.element);
+  }
+
+  static _toggleScreen(nextPage) {
+    if (nextPage) {
+      mainContainer.innerHTML = ``;
+      mainContainer.appendChild(nextPage);
+    }
   }
 }
